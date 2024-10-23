a1907d57d2a469aa4dcfe95d659ad356f91dcf78
Wesam <97985709+wesamAlsaleh@users.noreply.github.com>
2024-10-23 22:25:21 +0300

    Updated Factory/Seeder for the user

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,9 @@ class User extends Authenticatable
         'email',
         'password',
         'role',
+        'status',
+        'latitude',
+        'longitude'
     ];
 
     /**
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,9 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'role' => fake()->randomElement(['driver', 'admin']),
+            'status' => fake()->randomElement(['available', 'offline', 'busy']), // Random status
+            'latitude' => fake()->latitude(), // Generate a random latitude
+            'longitude' => fake()->longitude(), // Generate a random longitude
             'remember_token' => Str::random(10),
         ];
     }
